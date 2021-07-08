ada2a2415a943dc31ed91678100f63e762985d84
ZiPengYe <zero.cirno@gmail.com>
2021-07-08 15:30:17 +0800

    update #167

--- #167 Two Sum II - Input array is sorted.js
+++ #167 Two Sum II - Input array is sorted.js
@@ -4,14 +4,16 @@
  * @return {number[]}
  */
 const twoSum = (numbers, target) => {
-  for (let i = 0, j = numbers.length - 1; i < j;) {
-    const sum = numbers[i] + numbers[j];
-    if (sum === target) {
-      return [i + 1, j + 1];
-    } else if (sum < target) {
-      i++;
+  let [left, right] = [0, numbers.length - 1];
+  while (left < right) {
+    const total = numbers[left] + numbers[right];
+    if (total === target) {
+      return [left + 1, right + 1];
+    } else if (total < target) {
+      ++left;
     } else {
-      j--;
+      --right;
     }
   }
+  return [-1, -1];
 };
