693372499bc46f10626ced0275396bfb90711026
ZiPengYe <zero.cirno@gmail.com>
2021-07-08 15:05:07 +0800

    update #189

--- #189 Rotate Array.js
+++ #189 Rotate Array.js
@@ -1,11 +1,24 @@
 /**
+ * @param {any[]} nums 
+ * @param {number} start 
+ * @param {number} end 
+ * @return {void}
+ */
+const reverse = (nums, start, end) => {
+  while (start < end) {
+    [nums[start], nums[end]] = [nums[end], nums[start]];
+    ++start;
+    --end;
+  }
+};
+/**
  * @param {number[]} nums
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const rotate = (nums, k) => {
-  while (k) {
-    nums.unshift(nums.pop());
-    k--;
-  }
+  k %= nums.length;
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
 };
