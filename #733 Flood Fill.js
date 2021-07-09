34c649957927e9d1f3391961f8074e45d529ce76
ZiPengYe <zero.cirno@gmail.com>
2021-07-09 13:42:41 +0800

    update #733

--- #733 Flood Fill.js
+++ #733 Flood Fill.js
@@ -5,17 +5,32 @@
  * @param {number} newColor
  * @return {number[][]}
  */
-const floodFill = (image, sr, sc, newColor, oldColor) => {
-  if (typeof oldColor === 'undefined') {
-    if (newColor === image[sr][sc]) return image;
-    oldColor = image[sr][sc];
-  }
-  if (image[sr][sc] === oldColor) {
-    image[sr][sc] = newColor;
-    if (sr) floodFill(image, sr - 1, sc, newColor, oldColor);
-    if (sc) floodFill(image, sr, sc - 1, newColor, oldColor);
-    if (image.length - sr > 1) floodFill(image, sr + 1, sc, newColor, oldColor);
-    if (image[0].length - sc > 1) floodFill(image, sr, sc + 1, newColor, oldColor);
+const floodFill = (image, sr, sc, newColor) => {
+  const [r, c] = [image.length, image[0].length];
+  const currColor = image[sr][sc];
+
+  const dfs = (x, y) => {
+    if (image[x][y] === currColor) {
+      image[x][y] = newColor;
+      for ([x1, y1] of [
+        [x - 1, y], // top
+        [x + 1, y], // bottom
+        [x, y - 1], // left
+        [x, y + 1], // right
+      ]) {
+        if (
+          0 <= x1 && x1 < r &&
+          0 <= y1 && y1 < c &&
+          image[x1][y1] === currColor
+        ) {
+          dfs(x1, y1);
+        }
+      }
+    }
+  };
+
+  if (currColor !== newColor) {
+    dfs(sr, sc);
   }
   return image;
 };
