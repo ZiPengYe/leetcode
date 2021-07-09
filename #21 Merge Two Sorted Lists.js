a8cc81520ebbe930853d18952484c2a3ad93e345
ZiPengYe <zero.cirno@gmail.com>
2021-07-09 18:07:04 +0800

    update #21

--- #21 Merge Two Sorted Lists.js
+++ #21 Merge Two Sorted Lists.js
@@ -11,18 +11,17 @@
  * @return {ListNode}
  */
 const mergeTwoLists = function(l1, l2) {
-  const prehead = { next: null };
-  let prev = prehead;
-  while(l1 && l2) {
-    if (l1.val <= l2.val) {
-      prev.next = l1;
-      l1 = l1.next;
-    } else {
-      prev.next = l2;
-      l2 = l2.next;
-    }
-    prev = prev.next;
+  if (l1 === null) {
+    return l2;
   }
-  prev.next = l1 || l2;
-  return prehead.next;
+  if (l2 === null) {
+    return l1;
+  }
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l2.next, l1);
+    return l2;
+  }
 };
