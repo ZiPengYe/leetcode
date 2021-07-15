eff73dafdac0118c1ad28fe427f5864b326db8c0
ZiPengYe <zero.cirno@gmail.com>
2021-07-15 16:54:27 +0800

    update #70

--- #70 Climbing Stairs.js
+++ #70 Climbing Stairs.js
@@ -1,18 +1,13 @@
-// 记忆
-const store = [];
+const store = {};
 /**
- * 理解成下楼梯, n为总阶数, i为当前阶数
  * @param {number} n
- * @param {number} i
  * @return {number}
  */
-const climbStairs = function(n, i = n) {
-  if (i < 0) return 0;
-  // 刚好到达 0阶
-  if (i === 0) return 1;
-  // 读取记忆
-  if (store[i]) return store[i];
-  // 存储记忆, 模拟 下1阶, 下2阶
-  store[i] = climbStairs(n, i - 1) + climbStairs(n, i - 2);
-  return store[i];
+const climbStairs = function(n) {
+  if (store[n]) return store[n];
+  let p = 0, q = 0, r = 1;
+  for (let i = 1; i <= n; ++i) {
+    [p, q, r] = [q, r, r + q];
+  }
+  return store[n] = r;
 };
