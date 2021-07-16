6ed0338f257149e9ac1d9feed8874e7bbefbcb59
ZiPengYe <zero.cirno@gmail.com>
2021-07-16 17:33:54 +0800

    update #844

--- #844 Backspace String Compare.js
+++ #844 Backspace String Compare.js
@@ -4,24 +4,52 @@
  * @return {boolean}
  */
 const backspaceCompare = (S, T) => {
-  for (
-    let i = S.length - 1, j = T.length - 1;
-    i >= 0 || j >= 0;
-    i--, j--
-  ) {
-    let sb = 0;
-    let tb = 0;
-    while (S[i] === '#' || sb) {
-      if (S[i] === '#') sb++;
-      else sb--;
-      i--;
+  let i = S.length - 1,
+    j = T.length - 1;
+  // 记录退格数量
+  let s = t = 0;
+
+  while (i >= 0 || j >=0) {
+    
+    // 处理 S 的退格操作
+    while (i >= 0) {
+      if (S[i] === '#') {
+        // 是退格符
+        ++s;
+        --i;
+      } else if (s > 0) {
+        // 退格
+        --s;
+        --i;
+      } else {
+        break;
+      }
     }
-    while (T[j] === '#' || tb) {
-      if (T[j] === '#') tb++;
-      else tb--;
-      j--;
+    
+    // 处理 T 的退格操作
+    while (j >= 0) {
+      if (T[j] === '#') {
+        ++t;
+        --j;
+      } else if (t > 0) {
+        --t;
+        --j;
+      } else {
+        break;
+      }
     }
-    if (S[i] !== T[j]) return false;
+
+    if (i >= 0 && j >= 0) {
+      // 都还有字符, 判断当前字符
+      if (S[i] !== T[j]) {
+        return false;
+      }
+    } else if (i >= 0 || j >= 0) {
+      // 其中一个有字符
+      return false;
+    }
+    --i;
+    --j;
   }
   return true;
 };
