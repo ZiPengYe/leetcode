8002f4a6094210976418d37fdc9b813d4f17a1ba
ZiPengYe <zero.cirno@gmail.com>
2021-08-12 18:06:22 +0800

    update #49

--- #49 Group Anagrams.js
+++ #49 Group Anagrams.js
@@ -1,21 +1,21 @@
+const A_CHAR_CODE = 'a'.charCodeAt();
+
 /**
  * @param {string[]} strs
  * @return {string[][]}
  */
 const groupAnagrams = (strs) => {
-  const map = {};
-  return strs.reduce((acc, cur) => {
-    let value = 0;
-    for (let i = 0; i < cur.length; i++) {
-      value += (cur.codePointAt(i) - 64) ** 5;
+  const map = new Map();
+
+  for (const str of strs) {
+    const count = Array(26).fill(0);
+    for (const char of str) {
+      ++count[char.charCodeAt() - A_CHAR_CODE];
     }
-    value *= cur.length;
-    let index = map[value];
-    if (typeof index === 'undefined') {
-      index = map[value] = acc.length;
-    }
-    if (!acc[index]) acc[index] = [];
-    acc[index].push(cur);
-    return acc;
-  }, []);
+    const countStr = count.toString();
+    if (!map.has(countStr)) map.set(countStr, []);
+    map.get(countStr).push(str);
+  }
+
+  return Array.from(map.values());
 };
